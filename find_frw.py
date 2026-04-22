466977afaa74fae6ebcfc66c9304e5e1eba4c645
RangeyRover <RangeyRover@users.noreply.github.com>
2026-04-22 22:52:19 +0930

    chore: purge experimental noise files from source control

--- find_frw.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as ET
-
-tree = ET.parse('../cdf-hex-map.xml')
-for sec in tree.getroot().find('parameters'):
-    for grp in sec:
-        for blk in grp:
-            if blk.attrib['name'] == 'FRWAft':
-                print(f"FRWAft is in Section: {sec.attrib['name']}, Group: {grp.attrib['name']}")
--- test_notes.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-
-tree = ET.parse('../cdf-hex-map.xml')
-notes = tree.getroot().findall('.//notes')
-print("--- Multiline Notes ---")
-multiline_notes = [n.text for n in notes if n.text and '\n' in n.text]
-for text in multiline_notes[:5]:
-    print(text)
-    print("-----")
